86969849378b0cffb9917fdefd7a7fc855561ad9
Pragy Agarwal <agar.pragy@gmail.com>
2020-02-18 23:53:42 +0530

    Basic Models done

---
+++ src/main/java/com/psych/game/model/Admin.java
@@ -0,0 +1,11 @@
+package com.psych.game.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="admins")
+public class Admin extends Employee {
+
+}
---
+++ src/main/java/com/psych/game/model/Question.java
@@ -0,0 +1,41 @@
+package com.psych.game.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "questions")
+public class Question extends Auditable {
+    @NotNull
+    @Getter
+    @Setter
+    private String question;
+    @NotNull
+    @Getter
+    @Setter
+    private String correctAnswer;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "question")
+    @Getter
+    @Setter
+    private Set<EllenAnswer> ellenAnswers = new HashSet<>();
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    @Getter
+    @Setter
+    private GameMode gameMode;
+
+    public Question() {}
+
+    public Question(@NotNull String question, @NotNull String correctAnswer, @NotNull GameMode gameMode) {
+        this.question = question;
+        this.correctAnswer = correctAnswer;
+        this.gameMode = gameMode;
+    }
+}
